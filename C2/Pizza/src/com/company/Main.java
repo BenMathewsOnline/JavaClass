4c2bc108d00256b09f6b36705a7e4e933c6cd2d5
BenMathewsOnline <Ben.MathewsOnline@gmail.com>
2019-05-14 15:49:45 -0500

    initial check in

---
+++ C2/Pizza/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        double pizza = 13^2;
+        double pizzaD = (pizza * 10)/6;
+        System.out.println("A 10 Pizza will yield " + pizzaD + " slices");
+
+    }
+}
