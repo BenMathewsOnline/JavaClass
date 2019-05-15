65e859da14a55489bf118d9c8257c9e69fb30474
BenMathewsOnline <Ben.MathewsOnline@gmail.com>
2019-05-15 16:37:48 -0500

    finished gold

--- C2/Pizza/src/com/company/Main.java
+++ C2/Pizza/src/com/company/Main.java
@@ -4,9 +4,13 @@ public class Main {
 
     public static void main(String[] args)
     {
-        double pizza = 13^2;
-        double pizzaD = (pizza * 10)/6;
-        System.out.println("A 10 Pizza will yield " + pizzaD + " slices");
+       double pizzaD = (3.14 * 25);
+       double pizza = pizzaD /13;
+       System.out.println("A 10 Pizza will yield " + (int)pizza + " 13\" slices");
+
+       double pizzaD2 = (3.14 * 36);
+       double pizza2 = pizzaD2 / 13;
+       System.out.println("A 12 Pizza will yield " + (int)pizza2 + " 13\" slices");
 
     }
 }
