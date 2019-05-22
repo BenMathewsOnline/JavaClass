0f2e9654c4bad4fc1d9ee430f1a87137b3b253e5
BenMathewsOnline <Ben.MathewsOnline@gmail.com>
2019-05-22 09:56:14 -0500

    commit

---
+++ C4/DoWhileDemo/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Start of program");
+        int number = 1;
+
+        do {
+        System.out.println(number);
+        number++;
+    }
+        while (number <= 10);
+        System.out.println("End of program");
+    }
+}
