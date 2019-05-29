ea366f488e7c0e3d7287ac427de51297e1e81136
BenMathewsOnline <Ben.MathewsOnline@gmail.com>
2019-05-29 09:43:47 -0500

    Commit

---
+++ C6/People/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Person newPerson = new Person();
+        newPerson.setFirstName("Benjamin");
+        newPerson.setLastName("Mathews");
+
+        newPerson.getFullName();
+    }
+}
